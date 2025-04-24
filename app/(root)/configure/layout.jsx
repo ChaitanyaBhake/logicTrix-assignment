a6220c9b4d383d53baa8b694673b86279e1df5d8
Chaitanya Bhake <chaitubhake2013@gmail.com>
2025-04-24 21:02:49 +0530

    Added home route

--- app/(root)/configure/layout.jsx
+++ app/(root)/configure/layout.jsx
@@ -1,14 +1,20 @@
+"use client"
 import { X } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const layout = ({ children }) => {
+  const router = useRouter();
   return (
     <div className="bg-white rounded-lg w-screen max-w-screen">
       <div className="flex items-center justify-between p-6 border-b">
         <h2 className="text-xl font-semibold text-blue-600">
           CONFIGURE WELCOME MESSAGE
         </h2>
-        <button className="text-blue-600 hover:text-gray-700">
+        <button
+          className="text-blue-600 hover:text-gray-700 cursor-pointer"
+          onClick={() => router.push('/')}
+        >
           <X className="h-5 w-5" />
         </button>
       </div>
